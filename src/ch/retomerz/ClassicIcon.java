1bc5da1a725fd451095c1037f0ce66ab947b0d55
Reto Merz <reto.merz@gmail.com>
2016-06-17 22:36:32 +0200

    Improved error handling.

--- src/ch/retomerz/ClassicIcon.java
+++ src/ch/retomerz/ClassicIcon.java
@@ -1,19 +1,19 @@
 /**
- * This file is part of ClassIcon, licensed under the MIT License (MIT).
- * <p>
+ * This file is part of Class Icon, licensed under the MIT License (MIT).
+ *
  * Copyright (c) Reto Merz
  * Copyright (c) JetBrains s.r.o.
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,14 +24,19 @@
  */
 package ch.retomerz;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.text.StringUtil;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import java.awt.Frame;
 import java.awt.Image;
+import java.awt.MediaTracker;
+import java.io.File;
 
 public class ClassicIcon extends AbstractProjectComponent {
 
@@ -50,8 +55,27 @@ public class ClassicIcon extends AbstractProjectComponent {
       ce = "_CE";
     }
     if (imgCustom == null) {
-      if (!StringUtil.isEmptyOrSpaces(System.getProperty("classic.icon"))) {
-        imgCustom = new ImageIcon(System.getProperty("classic.icon")).getImage();
+      String custom = System.getProperty("classic.icon");
+      if (!StringUtil.isEmptyOrSpaces(custom)) {
+        File file = new File(custom);
+        if (file.exists()) {
+          if (file.isFile()) {
+            if (file.canRead()) {
+              ImageIcon icon = new ImageIcon(file.getPath());
+              if (MediaTracker.ERRORED == icon.getImageLoadStatus()) {
+                showWarning(String.format("File '%s' is not a image (for example a .png). Please fix 'classic.icon' (in IDEA_HOME/bin/idea.properties)", file));
+              } else {
+                imgCustom = icon.getImage();
+              }
+            } else {
+              showWarning(String.format("File '%s' is not readable. Please fix 'classic.icon' (in IDEA_HOME/bin/idea.properties)", file));
+            }
+          } else {
+            showWarning(String.format("Path '%s' is not a file. Please fix 'classic.icon' (in IDEA_HOME/bin/idea.properties)", file));
+          }
+        } else {
+          showWarning(String.format("File '%s' does not exists. Please fix 'classic.icon' (in IDEA_HOME/bin/idea.properties)", file));
+        }
       }
     }
     for (Frame frame : JFrame.getFrames()) {
@@ -74,5 +98,17 @@ public class ClassicIcon extends AbstractProjectComponent {
       }
       frame.setIconImage(use);
     }
+  }
+
+  private void showWarning(String message) {
+    if (SystemInfo.isWindows) {
+      message += "<br><br>" + "Note that you need to use '\\\\' as file separator in idea.properties (and not only a single '\\')";
+    }
+    new Notification(
+            "Classic Icon",
+            "Classic Icon",
+            message,
+            NotificationType.WARNING
+    ).setImportant(false).notify(myProject);
   }
 }
